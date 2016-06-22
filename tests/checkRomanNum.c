d74e53edd3eef0aa57aec53261d30eed933ca3bc
apekshak <apekshakumavat@gmail.com>
2016-06-21 21:33:35 -0400

    Added Check files for unit testing

---
+++ tests/checkRomanNum.c
@@ -0,0 +1,18 @@
+#include <check.h>
+#include "../src/romanNumeralCalc.h"
+
+/*
+START_TEST(test_romanNumeralCalc_create)
+{
+    romanNumeralCalc *r;
+    m = romanNumeralCalc_create();
+}
+END_TEST
+*/
+
+
+ int main(void)
+ {
+     return 0;
+ }
+
